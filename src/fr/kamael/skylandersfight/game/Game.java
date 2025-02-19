b4c2fb179f871530697b92d4acc43bf6f5aa017f
KamaelLeRandom <KamaelLeRandom@gmail.com>
2025-02-19 15:13:10 +0100

    feat: add new class GameState

--- src/fr/kamael/skylandersfight/game/Game.java
+++ src/fr/kamael/skylandersfight/game/Game.java
@@ -4,4 +4,16 @@ import fr.kamael.skylandersfight.Plugin;
 
 public class Game {
 	protected Plugin plugin = Plugin.plugin;
+	protected GameState state;
+	
+	public void setState(GameState state) {
+		this.state = state;
+	}
+	
+	public Boolean isState(GameState state) {
+		if (this.state.equals(state)) {
+			return true;
+		}
+		return false;
+	}
 }
---
+++ src/fr/kamael/skylandersfight/game/GameState.java
@@ -0,0 +1,8 @@
+package fr.kamael.skylandersfight.game;
+
+public enum GameState {
+	WAITING, // Quand la partie est configuré.
+	CHOOSING, // Quand les joueurs selectionnent leur Skylanders.
+	FIGHTING, // Quand une manche est cours.
+	ENDING, // Quand la partie est fini.
+}
