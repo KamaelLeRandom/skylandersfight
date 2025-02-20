34b1db8bb1460458a5c3d7d7359a0c149a8822e3
KamaelLeRandom <KamaelLeRandom@gmail.com>
2025-02-20 18:40:46 +0100

    feat: add new class Config

---
+++ src/fr/kamael/skylandersfight/game/config/Config.java
@@ -0,0 +1,215 @@
+package fr.kamael.skylandersfight.game.config;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import fr.kamael.skylandersfight.Constants;
+import fr.kamael.skylandersfight.utils.manager.ItemManager;
+
+public class Config {
+	private Inventory inventoryConfig;
+	private Integer nbPointWin = 3;
+	private Integer nbLifebar = 5;
+	private Integer nbSkylanderLine = 4;
+	private Integer timerDM = 5;
+	private Boolean activeHeal = true;
+	private Boolean activeDeathmatch = true;
+	private Boolean activeItem = true;
+	private Boolean activeBonusMap = true;
+	private Boolean activeEventMap = true;
+	
+	public Config() {
+		Inventory inv = Bukkit.createInventory(null, 36, Constants.inventoryConfigName);
+		for (Integer i = 0; i < 9; i++) {
+			inv.setItem(i     , ItemManager.getInventoryGlass());
+			inv.setItem(i + 18, ItemManager.getInventoryGlass());
+		}
+		inv.setItem( 4, ItemManager.makeBasicItem(Material.SLIME_BALL, "§aCommencer", 1));
+		inv.setItem( 9, ItemManager.makeBasicItem(Material.OAK_SIGN, "§7Nombre de ligne de Skylanders", this.nbSkylanderLine));
+		inv.setItem(10, ItemManager.makeBasicItem(Material.OAK_SIGN, "§7Nombre de point", this.nbPointWin));
+		inv.setItem(11, ItemManager.makeBasicItem(Material.OAK_SIGN, "§7Nombre de barre de vie", this.nbLifebar));
+		inv.setItem(12, ItemManager.makeBasicItem(Material.OAK_SIGN, "§7Durée avant le Deathmatch", this.timerDM));
+		inv.setItem(18, ItemManager.makeBasicItem(Material.GREEN_TERRACOTTA, "§7Bloc de soin", 1));
+		inv.setItem(19, ItemManager.makeBasicItem(Material.GREEN_TERRACOTTA, "§7Deathmatch", 1));
+		inv.setItem(20, ItemManager.makeBasicItem(Material.GREEN_TERRACOTTA, "§7Objets Aléatoire", 1));
+		inv.setItem(21, ItemManager.makeBasicItem(Material.GREEN_TERRACOTTA, "§7Bonus élémentaire", 1));
+		inv.setItem(22, ItemManager.makeBasicItem(Material.GREEN_TERRACOTTA, "§7Événement d'Arène", 1));
+		
+		this.inventoryConfig = inv;
+	}
+	
+	public Inventory getInventory() {
+		return this.inventoryConfig;
+	}
+	
+	public Integer getNbPointWin() {
+		return this.nbPointWin;
+	}
+	
+	public Integer updateNbPointWin(InventoryAction action) {
+		switch (action) {
+			case PICKUP_ALL: {
+				if (this.nbPointWin > 1)
+					this.nbPointWin--;
+				break;
+			}
+			case PICKUP_HALF: {
+				if (this.nbPointWin < 10)
+					this.nbPointWin++;
+				break;
+			}
+			default:
+				break;
+		}
+		return this.nbPointWin;
+	}
+	
+	public Integer getNbLifebar() {
+		return this.nbLifebar;
+	}
+	
+	public Integer updateNbLifebar(InventoryAction action) {
+		switch (action) {
+			case PICKUP_ALL: {
+				if (this.nbLifebar > 1)
+					this.nbLifebar--;
+				break;
+			}
+			case PICKUP_HALF: {
+				if (this.nbLifebar < 5)
+					this.nbLifebar++;
+				break;
+			}
+			default:
+				break;
+		}
+		return this.nbLifebar;
+	}
+
+	public Integer getNbSkylanderLine() {
+		return this.nbSkylanderLine;
+	}
+	
+	public Integer updateNbSkylanderLine(InventoryAction action) {
+		switch (action) {
+			case PICKUP_ALL: {
+				if (this.nbSkylanderLine > 1)
+					this.nbSkylanderLine--;
+				break;
+			}
+			case PICKUP_HALF: {
+				if (this.nbSkylanderLine < 4)
+					this.nbSkylanderLine++;
+				break;
+			}
+			default:
+				break;
+		}
+		return this.nbSkylanderLine;
+	}
+	
+	public Integer getTimerDM() {
+		return this.timerDM;
+	}
+	
+	public Integer updateTimerDM(InventoryAction action) {
+		switch (action) {
+			case PICKUP_ALL: {
+				if (this.timerDM > 3)
+					this.timerDM--;
+				break;
+			}
+			case PICKUP_HALF: {
+				if (this.timerDM < 10)
+					this.timerDM++;
+				break;
+			}
+			default:
+				break;
+		}
+		return this.timerDM;
+	}
+
+	public Boolean getActiveHeal() {
+		return this.activeHeal;
+	}
+	
+	public void updateActiveHeal(ItemStack item) {
+		this.activeHeal = !this.activeHeal;
+		
+		if (this.activeHeal) {
+			item.setType(Material.GREEN_TERRACOTTA);
+		} else {
+			item.setType(Material.RED_TERRACOTTA);
+		}
+		
+		return;
+	}
+
+	public Boolean getActiveDeathmatch() {
+		return this.activeDeathmatch;
+	}
+	
+	public void updateActiveDeathmatch(ItemStack item) {
+		this.activeDeathmatch = !this.activeDeathmatch;
+		
+		if (this.activeDeathmatch) {
+			item.setType(Material.GREEN_TERRACOTTA);
+		} else {
+			item.setType(Material.RED_TERRACOTTA);
+		}
+		
+		return;
+	}
+	
+	public Boolean getActiveItem() {
+		return this.activeItem;
+	}
+	
+	public void updateActiveItem(ItemStack item) {
+		this.activeItem = !this.activeItem;
+		
+		if (this.activeItem) {
+			item.setType(Material.GREEN_TERRACOTTA);
+		} else {
+			item.setType(Material.RED_TERRACOTTA);
+		}
+		
+		return;
+	}
+	
+	public Boolean getActiveBonusMap() {
+		return this.activeBonusMap;
+	}
+	
+	public void updateActiveBonusMap(ItemStack item) {
+		this.activeBonusMap = !this.activeBonusMap;
+		
+		if (this.activeBonusMap) {
+			item.setType(Material.GREEN_TERRACOTTA);
+		} else {
+			item.setType(Material.RED_TERRACOTTA);
+		}
+		
+		return;
+	}
+
+	public Boolean getActiveEventMap() {
+		return this.activeEventMap;
+	}
+	
+	public void updateActiveEventMap(ItemStack item) {
+		this.activeEventMap = !this.activeEventMap;
+		
+		if (this.activeEventMap) {
+			item.setType(Material.GREEN_TERRACOTTA);
+		} else {
+			item.setType(Material.RED_TERRACOTTA);
+		}
+		
+		return;
+	}
+}
--- src/fr/kamael/skylandersfight/utils/manager/ItemManager.java
+++ src/fr/kamael/skylandersfight/utils/manager/ItemManager.java
@@ -2,7 +2,9 @@ package fr.kamael.skylandersfight.utils.manager;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 public class ItemManager {
@@ -14,5 +16,29 @@ public class ItemManager {
 		meta.setOwner(p.getName());
 		skull.setItemMeta(meta);
 		return skull;
+	}
+	
+	public static ItemStack getInventoryGlass() {
+		ItemStack it = new ItemStack(Material.GRAY_STAINED_GLASS_PANE, 1);
+		ItemMeta itM = it.getItemMeta();
+		itM.setDisplayName("§7-");
+		itM.setUnbreakable(true);
+		itM.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		itM.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+		itM.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		it.setItemMeta(itM);
+		return it;
+	}
+	
+	public static ItemStack makeBasicItem(Material material, String name, Integer number) {
+		ItemStack it = new ItemStack(material, number);
+		ItemMeta itM = it.getItemMeta();
+		itM.setDisplayName(name);
+		itM.setUnbreakable(true);
+		itM.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		itM.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+		itM.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		it.setItemMeta(itM);
+		return it;
 	}
 }
