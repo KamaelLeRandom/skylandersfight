b2056b12d767c523291d3557d95379cdacbf8498
KamaelLeRandom <KamaelLeRandom@gmail.com>
2025-02-20 13:39:55 +0100

    feat: add new class GameTeam

---
+++ src/fr/kamael/skylandersfight/game/GameTeam.java
@@ -0,0 +1,47 @@
+package fr.kamael.skylandersfight.game;
+
+import java.util.ArrayList;
+
+public class GameTeam {
+	private String name;
+	private Integer nbPoint;
+	private Integer nbKill;
+	private ArrayList<GamePlayer> players;
+	
+	public GameTeam(String name, ArrayList<GamePlayer> players) {
+		this.name = name;
+		this.nbPoint = 0;
+		this.nbKill = 0;
+		this.players = players;
+		
+		for (GamePlayer player : players) {
+			player.setInitialTeam(this);
+		}
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public Integer getNbPoint() {
+		return this.nbPoint;
+	}
+	
+	public Integer updateNbPoint(Integer value) {
+		this.nbPoint += value;
+		return this.nbPoint;
+	}
+	
+	public Integer getNbKill() {
+		return this.nbKill;
+	}
+	
+	public Integer updateNbKill(Integer value) {
+		this.nbKill += value;
+		return this.nbKill;
+	}
+	
+	public ArrayList<GamePlayer> getPlayers() {
+		return this.players;
+	}
+}
