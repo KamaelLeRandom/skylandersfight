29eb6396c38c975422423ff0eb13d8ceff1d5a8d
KamaelLeRandom <KamaelLeRandom@gmail.com>
2025-02-20 16:35:52 +0100

    feat: implementation of GamePlayer and GameTeam in Game

--- src/fr/kamael/skylandersfight/game/Game.java
+++ src/fr/kamael/skylandersfight/game/Game.java
@@ -1,10 +1,16 @@
 package fr.kamael.skylandersfight.game;
 
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
 import fr.kamael.skylandersfight.Plugin;
 
 public class Game {
 	protected Plugin plugin = Plugin.plugin;
 	protected GameState state;
+	protected ArrayList<GamePlayer> listPlayers;
+	protected ArrayList<GameTeam> listTeams;
 	
 	public void setState(GameState state) {
 		this.state = state;
@@ -15,5 +21,22 @@ public class Game {
 			return true;
 		}
 		return false;
+	}
+	
+	public GamePlayer getPlayer(Player player) {
+		for (GamePlayer gamePlayer : this.listPlayers) {
+			if (gamePlayer.getPlayer().equals(player)) {
+				return gamePlayer;
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<GamePlayer> getPlayers() {
+		return this.listPlayers;
+	}
+	
+	public ArrayList<GameTeam> getTeams() {
+		return this.listTeams;
 	}
 }
